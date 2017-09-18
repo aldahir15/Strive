e98c36d996052f9cb5d8bf2f35a569f81b29bbd0
Jose Aldahir Moreno <jamoren1@uci.edu>
2017-09-18 09:51:04 -0700

    Finished Users and Sessions controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,19 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      login(@user)
+      render :show
+    else
+      render json: "invalid login credentials"
+    end
+  end
+
+  def destroy
+    if current_user
+      logout
+    else
+      render :json => {:error => "Not Logged In"}.to_json, :status => 404
+    end
+  end
+end
