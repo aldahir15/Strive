f98db056a27349785a3ddb12fbeb5a1039916a73
Jose Aldahir Moreno <jamoren1@uci.edu>
2017-09-18 10:05:49 -0700

    Wrote entry jsx file, static_pages root, and routes

---
+++ frontend/strive.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from "./store/store";
+import Root from './components/root';
+import {login} from "./actions/session_actions";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
+  const root = document.findElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
+});
