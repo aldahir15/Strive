25a05951a2fc5f65468671ca97f5afa1df4560ec
Jose Aldahir Moreno <jamoren1@uci.edu>
2017-09-18 09:40:41 -0700

    Fixed error in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   validates :username, :password_digest, :session_token, presence: true
   validates :password, length: {minimum: 6, allow_nil: true}
   after_initialize :ensure_session_token
-  attr_reader: password
+  attr_reader :password
 
   def self.find_by_credentials(username, password)
     @user = User.find_by_username(username)
