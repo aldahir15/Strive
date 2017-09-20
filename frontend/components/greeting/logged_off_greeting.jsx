fe3d8c2f213b77f6aebf993495995a9ce8103d75
Jose Aldahir Moreno <jamoren1@uci.edu>
2017-09-20 14:16:22 -0700

    Moved all assets to flickr

--- frontend/components/greeting/logged_off_greeting.jsx
+++ frontend/components/greeting/logged_off_greeting.jsx
@@ -6,7 +6,7 @@ import Modal from 'react-modal';
 const render = () => {
   return (<div className="logged-off-greeting">
     <div className="slogan">
-      <img src="/assets/logo.png" />
+      <img src="https://c1.staticflickr.com/5/4380/36541394753_bb925e4c1c_o.png" />
       <h3>Helping you </h3>
       <h3>become a better you</h3>
 
